8efe8744fc3482d0aff2048a8c6430f2d98ed59d
jaeju.jang <jjj0611@gmail.com>
2020-02-07 18:34:53 +0900

    사용자 입력을 처리하기 위한 Input class를 생성했습니다.

---
+++ src/main/java/input/LottoInputScanner.java
@@ -0,0 +1,20 @@
+package input;
+
+import java.util.Scanner;
+
+public class LottoInputScanner {
+
+    private static final String PURCHASE_LOTTO_PRICE_STATEMENT = "구입 금액을 입력해주세요.";
+    private static final String ASK_LAST_LOTTERY_NUMBER_STATEMENT = "지난주 당첨 번호를 입력해주세요.";
+    private static final Scanner inputScanner = new Scanner(System.in);
+
+    public static String askLottoPurchacePrice() {
+        System.out.println(PURCHASE_LOTTO_PRICE_STATEMENT);
+        return inputScanner.nextLine();
+    }
+
+    public static String askLottoWinningNumber() {
+        System.out.println(ASK_LAST_LOTTERY_NUMBER_STATEMENT);
+        return inputScanner.nextLine();
+    }
+}

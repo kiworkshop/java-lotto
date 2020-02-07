7cdb93675ca129338ee86e581c6828ff7c9e8e0a
jaeju.jang <jjj0611@gmail.com>
2020-02-07 18:55:43 +0900

    사용자의 보너스 번호 입력을 위한 메서드를 추가했습니다.

--- src/main/java/input/LottoInputScanner.java
+++ src/main/java/input/LottoInputScanner.java
@@ -6,6 +6,7 @@ public class LottoInputScanner {
 
     private static final String PURCHASE_LOTTO_PRICE_STATEMENT = "구입 금액을 입력해주세요.";
     private static final String ASK_LAST_LOTTERY_NUMBER_STATEMENT = "지난주 당첨 번호를 입력해주세요.";
+    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해주세요";
     private static final Scanner inputScanner = new Scanner(System.in);
 
     public static String askLottoPurchacePrice() {
@@ -15,6 +16,11 @@ public class LottoInputScanner {
 
     public static String askLottoWinningNumber() {
         System.out.println(ASK_LAST_LOTTERY_NUMBER_STATEMENT);
+        return inputScanner.nextLine();
+    }
+
+    public static String askBonusNumber() {
+        System.out.println(ASK_BONUS_NUMBER);
         return inputScanner.nextLine();
     }
 }

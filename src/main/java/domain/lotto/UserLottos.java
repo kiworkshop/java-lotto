9113da6cd1f55c5308183858c8ad0e0bf905c07e
jaeju.jang <jjj0611@gmail.com>
2020-02-07 18:39:02 +0900

    User들의 Lotto를 저장하는 UserLottos 클래스를 생성했습니다.

---
+++ src/main/java/domain/lotto/UserLottos.java
@@ -0,0 +1,17 @@
+package domain.lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserLottos {
+
+    List<Lotto> lottos = new ArrayList<>();
+
+    public static UserLottos from(List<Lotto> lottos) {
+        return new UserLottos(lottos);
+    }
+
+    private UserLottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}

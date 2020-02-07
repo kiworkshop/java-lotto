cfe7e5864b5e1c50f44fa6622ae3ce4e4e8b73e0
jaeju.jang <jjj0611@gmail.com>
2020-02-08 02:13:57 +0900

    Lotto 실행을 위한 controller를 생성하고 메서드 추가

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,90 @@
+package controller;
+
+import domain.lotto.Lotto;
+import domain.lotto.LottoFactory;
+import domain.lotto.LottoNumber;
+import domain.lotto.UserLottos;
+import domain.lotto.WinningLotto;
+import domain.money.Money;
+import domain.result.Rank;
+import domain.result.UserRanks;
+import input.LottoInputScanner;
+import output.LottoPrinter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LottoController {
+
+    public void run() {
+        Money money = getMoneyFromUser();
+        UserLottos userLottos = getUserLottos(money);
+        LottoPrinter.printUserLottos(userLottos);
+        WinningLotto winningLotto = getWinningLotto();
+        UserRanks userRanks = getUserRank(winningLotto, userLottos);
+        LottoPrinter.printUserRanks(userRanks);
+    }
+
+    private Money getMoneyFromUser() {
+        int moneyFromUser = parseMoneyToInt(inputMoneyFromUser());
+        return Money.from(moneyFromUser);
+    }
+
+    private String inputMoneyFromUser() {
+        return LottoInputScanner.askLottoPurchacePrice();
+    }
+
+    private int parseMoneyToInt(String inputMoney) {
+        try {
+            return Integer.parseInt(inputMoney);
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private UserLottos getUserLottos(Money money) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < money.getCountOfLotto(); i++) {
+            lottos.add(LottoFactory.publishLotto());
+        }
+
+        return UserLottos.from(lottos);
+    }
+
+    private WinningLotto getWinningLotto() {
+        Lotto winningLotto = parseInputToLotto(LottoInputScanner.askLottoWinningNumber());
+        LottoNumber bonusNumber = parseInputToBonusNumber(LottoInputScanner.askBonusNumber());
+        return WinningLotto.from(winningLotto, bonusNumber);
+    }
+
+    private Lotto parseInputToLotto(String input) {
+        List<String> lottoNumbers = Arrays.asList(input.split(","));
+        Set<LottoNumber> lottoNumberSet = lottoNumbers.stream()
+                .mapToInt(number -> Integer.parseInt(number.trim()))
+                .mapToObj(LottoNumber::from)
+                .collect(Collectors.toSet());
+
+        return Lotto.from(lottoNumberSet);
+    }
+
+    private LottoNumber parseInputToBonusNumber(String input) {
+        try {
+            return LottoNumber.from(Integer.parseInt(input));
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private UserRanks getUserRank(WinningLotto winningLotto, UserLottos userLottos) {
+        List<Lotto> lottos = userLottos.getLottos();
+        List<Rank> ranks = lottos.stream()
+                .map(winningLotto::getRank)
+                .collect(Collectors.toList());
+
+        return new UserRanks(ranks);
+    }
+
+}

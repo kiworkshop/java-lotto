b3e64ba43e0e16a58549fd711bdcb22bed1516bc
eunii <eoeh04@gmail.com>
2021-07-04 23:00:26 +0900

    feat: 1~45숫자중 중복되지 않게 6개숫자 뽑기 구현

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -1,7 +1,24 @@
 package domain;
 
+import org.graalvm.compiler.phases.common.inlining.info.elem.InlineableGraph;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoMachine {
+
+    private static List<Integer> balls = IntStream.range(1,45).boxed().collect(Collectors.toList());
+
     public int getLottoTickt(int givenMoney) {
         return givenMoney/1000;
+    }
+
+    public List<Integer> createRandomNumber() {
+        Collections.shuffle(balls);
+        List<Integer> lottoNumbers = balls.subList(0,6);
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
 }
--- src/test/java/domain/LottoMachineTest.java
+++ src/test/java/domain/LottoMachineTest.java
@@ -2,19 +2,32 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoMachineTest {
 
     @Test
     void 돈을_입력받으면_구매한_로또_수를_리턴한다() {
-
         //given
         int givenMoney = 14000;
-        //when
         LottoMachine lottoMachine = new LottoMachine();
+        //when
         int ticketNumber = lottoMachine.getLottoTickt(givenMoney);
         //than
         assertThat(ticketNumber).isEqualTo(14);
+    }
+
+    @Test
+    void _1부터_45사이의_숫자중_중복되지_않는_7개를_뽑아서_오름차순으로_리턴한다() {
+        //given
+        LottoMachine lottoMachine = new LottoMachine();
+        //when
+        List<Integer> lottoNumbers = lottoMachine.createRandomNumber();
+        //than
+        assertThat(lottoNumbers.size()).isEqualTo(6);
+        assertThat(lottoNumbers.stream().distinct().count()).isEqualTo(6);
+
     }
 }

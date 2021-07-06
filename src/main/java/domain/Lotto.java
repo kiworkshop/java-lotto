fc762b873993c569a78717e0370ce9b0e09546f7
Ariel429 <arielkim429@gmail.com>
2021-07-06 21:56:20 +0900

    Revert "commit"
    This reverts commit 03f67c8d8c56f3a23b1c155b2fed1a9f23b59204.

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Lotto {
+    List<Integer> lottoNumbers;
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+}

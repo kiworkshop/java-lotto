4a83ad012ca365f412e8c8818289e0de4428e476
chunghyeon-kim <asdddq11@gmail.com>
2021-07-07 19:09:13 +0900

    refactor: 테스트용 로또 클래스 생성

--- src/test/java/lotto/domain/PrizeCountTest.java
+++ src/test/java/lotto/domain/PrizeCountTest.java
@@ -10,42 +10,15 @@ import java.util.HashSet;
 
 public class PrizeCountTest {
 
-    private final String winningNumberInput = "1, 2, 3, 4, 5, 6";
-    private final String bonusNumberInput = "7";
-    private final WinningLotto winningLotto = new WinningLotto(winningNumberInput, bonusNumberInput);
-
     @Test
     @DisplayName("로또 여러 게임의 당첨 통계를 구한다")
     void testCountPrizesOfLottoSet() {
         //given
-        HashSet<Lotto> lottos = new HashSet<>();
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 6")));
-
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 7")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 7")));
-
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 10")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 10")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 5, 10")));
-
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 10, 11")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 10, 11")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 10, 11")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 4, 10, 11")));
-
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 10, 11, 12")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 10, 11, 12")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 10, 11, 12")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 10, 11, 12")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 3, 10, 11, 12")));
-
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 2, 10, 11, 12, 13")));
-        lottos.add(new Lotto(LottoParser.generateLotto("1, 10, 11, 12, 13, 14")));
-        lottos.add(new Lotto(LottoParser.generateLotto("19, 10, 11, 12, 13, 14")));
-        LottoSet lottoset = new LottoSet(lottos);
+        TestLottoSet lottoSet = new TestLottoSet();
+        TestWinningLotto winningLotto = new TestWinningLotto();
 
         //when
-        PrizeCount prizeCount = new PrizeCount(lottoset, winningLotto);
+        PrizeCount prizeCount = new PrizeCount(lottoSet, winningLotto);
 
         //then
         assertThat(prizeCount.getCountFirst()).isEqualTo(1);

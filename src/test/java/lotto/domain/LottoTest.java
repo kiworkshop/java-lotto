d1e674006653a846146e017aee21505ee7734b8c
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-07-19 18:09:54 +0900

    test: move tests from WinningLottoTest to LottoTest and add validation test cases

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,13 +1,122 @@
 package lotto.domain;
 
+import lotto.domain.dto.WinningLottoInput;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoTest {
+
+    @Test
+    @DisplayName("숫자가 6개인지 확인한다")
+    void testWinningNumberSize() {
+        // given
+        List<Integer> winningNumberInput = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6})
+                .boxed()
+                .collect(Collectors.toList());
+        Integer bonusNumberInput = 10;
+
+        // when
+        WinningLotto winningLotto = new WinningLotto(
+                new WinningLottoInput(winningNumberInput, bonusNumberInput)
+        );
+        int size = winningLotto.getLottoNumbers().size();
+
+        // then
+        assertThat(size).isEqualTo(Lotto.LOTTO_NUMBER_SIZE);
+    }
+
+    @Test
+    @DisplayName("45 초과인 값 포함할 때 예외를 던진다")
+    void testInputGreaterThanUpperBoundary() {
+        // given
+        List<Integer> input = Arrays.stream(new int[]{1, 2, 3, 4, 5, 46}).boxed().collect(Collectors.toList());
+        Integer bonus = 10;
+        WinningLottoInput winningLottoInput = WinningLottoInput.builder()
+                .numbers(input)
+                .bonus(bonus)
+                .build();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningLotto(winningLottoInput);
+        });
+    }
+
+    @Test
+    @DisplayName("1 미만인 값 포함할 때 예외를 던진다")
+    void testInputSmallerThanLowerBoundary() {
+        // given
+        List<Integer> input = Arrays.stream(new int[]{0, 2, 3, 4, 5, 6}).boxed().collect(Collectors.toList());
+        Integer bonus = 10;
+        WinningLottoInput winningLottoInput = WinningLottoInput.builder()
+                .numbers(input)
+                .bonus(bonus)
+                .build();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningLotto(winningLottoInput);
+        });
+    }
+
+    @Test
+    @DisplayName("6개 이하의 숫자가 입력됐을 때 예외를 던진다")
+    void testInputLengthLessThanStandard() {
+        // given
+        List<Integer> input = Arrays.stream(new int[]{1, 2, 3, 4, 5}).boxed().collect(Collectors.toList());
+        Integer bonus = 10;
+        WinningLottoInput winningLottoInput = WinningLottoInput.builder()
+                .numbers(input)
+                .bonus(bonus)
+                .build();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningLotto(winningLottoInput);
+        });
+    }
+
+    @Test
+    @DisplayName("6개 이상의 숫자가 입력됐을 때 예외를 던진다")
+    void testInputLengthGreaterThanStandard() {
+        // given
+        List<Integer> input = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6, 7}).boxed().collect(Collectors.toList());
+        Integer bonus = 10;
+        WinningLottoInput winningLottoInput = WinningLottoInput.builder()
+                .numbers(input)
+                .bonus(bonus)
+                .build();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningLotto(winningLottoInput);
+        });
+    }
+
+    @Test
+    @DisplayName("중복된 숫자가 입력됐을 때 예외를 던진다")
+    void testInputWithDuplicatedNumbers() {
+        // given
+        List<Integer> input = Arrays.stream(new int[]{1, 2, 3, 3, 5, 6}).boxed().collect(Collectors.toList());
+        Integer bonus = 10;
+        WinningLottoInput winningLottoInput = WinningLottoInput.builder()
+                .numbers(input)
+                .bonus(bonus)
+                .build();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WinningLotto(winningLottoInput);
+        });
+    }
 
     @Test
     void testEqualityOfLottos() {

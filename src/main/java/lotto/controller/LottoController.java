491f190a1851325b70da619cd7e9d01c24e22ff7
Chloe <page.prologue@gmail.com>
2021-08-18 04:07:06 +0900

    refactor : LottoService, LottoController, StringUtil 정리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,16 @@
 package lotto.controller;
 
 import lotto.domain.lotto.LottoTickets;
-import lotto.domain.util.StringUtil;
 import lotto.domain.vending.BuyingPrice;
 import lotto.domain.vending.TicketAmount;
 import lotto.domain.winning.WinningLottoRank;
 import lotto.domain.winning.WinningNumbers;
 import lotto.service.LottoService;
+import lotto.util.StringUtil;
 import lotto.view.InputView;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoController {
     private final LottoService lottoService = new LottoService();
@@ -17,6 +18,7 @@ public class LottoController {
     public BuyingPrice getBuyingPrice(String price) throws NumberFormatException {
         try {
             return new BuyingPrice(Integer.parseInt(price));
+
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("구매 금액은 숫자로 입력해 주세요.");
         }
@@ -27,25 +29,24 @@ public class LottoController {
             return new TicketAmount(totalTicketAmount, Integer.parseInt(manualTicketAmount));
 
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("수동 티켓 개수는 숫자로 입력해 주세요.");
+            throw new IllegalArgumentException("수동으로 구매할 티켓 개수를 확인해 주세요.");
         }
     }
 
     public LottoTickets issueTickets(TicketAmount ticketAmount) {
         List<String> inputManualNumbers = InputView.getManualNumbers(ticketAmount.manual());
-        return lottoService.issueTickets(ticketAmount.auto(), inputManualNumbers);
+        List<List<Integer>> manualNumbers = inputManualNumbers.stream()
+                .map(StringUtil::splitParseInt)
+                .collect(Collectors.toList());
+        return lottoService.issueTickets(ticketAmount.auto(), manualNumbers);
     }
 
     public WinningNumbers validateWinningLottoNumbers(String inputWinningNumbers, String inputBonusNumber) {
-        try {
-            List<Integer> splitWinningNumbers = StringUtil.splitParseInt(inputWinningNumbers);
-            int bonusNumber = Integer.parseInt(inputBonusNumber);
-            return new WinningNumbers(splitWinningNumbers, bonusNumber);
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("수동 로또 번호는은 숫자로 입력해 주세요.");
-        }
+        List<Integer> splitWinningNumbers = StringUtil.splitParseInt(inputWinningNumbers);
+        int bonusNumber = StringUtil.parseInt(inputBonusNumber);
+        return new WinningNumbers(splitWinningNumbers, bonusNumber);
     }
+
 
     public float calculateProfitRate(int totalTicketAmount, LottoTickets lottoTickets, WinningNumbers winningNumbers) {
         WinningLottoRank winningLottoRank = lottoService.getWinningLottoRank(lottoTickets, winningNumbers);
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,6 @@
 package lotto.service;
 
 import lotto.domain.lotto.LottoTickets;
-import lotto.domain.util.StringUtil;
 import lotto.domain.vending.LottoTicketVendingMachine;
 import lotto.domain.winning.WinningLottoRank;
 import lotto.domain.winning.WinningNumbers;
@@ -9,7 +8,6 @@ import lotto.domain.winning.WinningStatistics;
 import lotto.view.OutputView;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LottoService {
     private final LottoTicketVendingMachine vendingMachine;
@@ -18,26 +16,8 @@ public class LottoService {
         this.vendingMachine = new LottoTicketVendingMachine();
     }
 
-    public LottoTickets issueTickets(int autoTicketAmount, List<String> inputManualNumbers) {
-        List<List<Integer>> manualLottoNumbers = manualLottoNumbers(inputManualNumbers);
-        return vendingMachine.issueTickets(autoTicketAmount, manualLottoNumbers);
-    }
-
-    public List<List<Integer>> manualLottoNumbers(List<String> inputManualNumbers) {
-        return inputManualNumbers.stream()
-                .map(StringUtil::split)
-                .map(this::parseLottoNumbers)
-                .collect(Collectors.toList());
-    }
-
-    private List<Integer> parseLottoNumbers(List<String> inputManualNumbers) {
-        try {
-            return inputManualNumbers.stream()
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 숫자를 입력해 주세요.");
-        }
+    public LottoTickets issueTickets(int autoTicketAmount, List<List<Integer>> manualNumbers) {
+        return vendingMachine.issueTickets(autoTicketAmount, manualNumbers);
     }
 
     public float getProfitRate(int totalTicketAmount, WinningLottoRank winningLottoRank) {
@@ -46,13 +26,8 @@ public class LottoService {
     }
 
     public WinningLottoRank getWinningLottoRank(LottoTickets lottoTickets, WinningNumbers winningNumbers) {
-        try {
-            WinningLottoRank winningLottoRank = new WinningLottoRank(lottoTickets, winningNumbers);
-            OutputView.printWinningStatistics(winningLottoRank);
-            return winningLottoRank;
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 숫자를 입력해 주세요.");
-        }
+        WinningLottoRank winningLottoRank = new WinningLottoRank(lottoTickets, winningNumbers);
+        OutputView.printWinningStatistics(winningLottoRank);
+        return winningLottoRank;
     }
 }

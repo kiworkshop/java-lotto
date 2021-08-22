886645a0e76f59809f7d2b3087e67fba04786447
chunghyeon-kim <asdddq11@gmail.com>
2021-08-22 15:37:40 +0900

    refactor: Modify checking lottonumbers correspond logic

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,11 +6,12 @@ import lotto.domain.dto.WinningLottoInputDTO;
 import lotto.parser.LottoParser;
 import lotto.util.NumberValidateUtils;
 
-import java.util.Comparator;
+import java.util.HashSet;
 
 @Getter
 public class WinningLotto {
 
+    public static final int ALL_LOTTO_NUMBERS_OF_TWO_LOTTO_TICKET = 12;
     private final Lotto lotto;
     private final LottoNumber bonusNumber;
 
@@ -32,27 +33,11 @@ public class WinningLotto {
     }
 
     private int getMatchNumbersCount(Lotto targetLotto) {
-        targetLotto.getLottoNumbers().sort(Comparator.comparingInt(LottoNumber::getLottoNumber));
-        this.lotto.getLottoNumbers().sort(Comparator.comparingInt(LottoNumber::getLottoNumber));
-        int targetIdx = 0;
-        int winningIdx = 0;
-        int matchNumbersCount = 0;
-        while (targetIdx < targetLotto.lottoNumbers.size() && winningIdx < this.lotto.getLottoNumbers().size()) {
-            int targetNumber = targetLotto.lottoNumbers.get(targetIdx).getLottoNumber();
-            int winningNumber = this.lotto.getLottoNumbers().get(winningIdx).getLottoNumber();
-            if (targetNumber == winningNumber) {
-                matchNumbersCount++;
-                targetIdx++;
-                winningIdx++;
-            }
-            else if (targetNumber > winningNumber) {
-                winningIdx++;
-            }
-            else {
-                targetIdx++;
-            }
-        }
-        return matchNumbersCount;
+        HashSet<LottoNumber> distinctLottoNumbersSet = new HashSet<>();
+        distinctLottoNumbersSet.addAll(lotto.getLottoNumbers());
+        distinctLottoNumbersSet.addAll(targetLotto.getLottoNumbers());
+
+        return ALL_LOTTO_NUMBERS_OF_TWO_LOTTO_TICKET - distinctLottoNumbersSet.size();
     }
 
 }
